52d0f01acc52820746dbf32c7e7f8af80e065abc
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-07-18 15:09:37 +0700

    Complete responsive header + Complete slider + Build base story

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-coffee': '#A56634'
+        'coffee': {
+          50: '#e8d6d0',
+          200: '#A56634',
+          400: '#A25f4b',
+          600: '#744838'
+        }
       },
       fontFamily: {
         'Karla': ['Karla', 'sans-serif']
       },
       keyframes: {
         slideDown: {
-          '0': {transform: 'translateY(-100%)'},
-          '1': {transform: 'translateY(0)'}
+          '0%': {transform: 'translateY(-100%)'},
+          '100%': {transform: 'translateY(0)'}
         }
       },
       animation: {
-        slideDown: 'slideDown 1s ease-in-out'
+        slideDown: 'slideDown .3s ease-in-out'
+      },
+      backgroundImage: {
+        'slider-bg': 'url("./img/slider.jpg")'
       }
     },
   },
